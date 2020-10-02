d7d6275e58aad46f903806d0c9a7ea4b99b87af4
Bluesaxorcist <joshua@operatorfoundation.org>
2020-10-01 19:00:21 -0500

    started implementing OutputStream

--- app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowCipher.java
+++ app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowCipher.java
@@ -3,19 +3,26 @@ package org.operatorfoundation.shadow;
 import java.util.Random;
 
 public class ShadowCipher {
+    static int lengthWithTagSize = 2 + 16;
+    static int tagSizeBits = 16 * 8;
+    static int tagSize = 16;
+    static int maxPayloadSize = 16417;
+    //TODO(Could i set the saltSize static variable in a switch loop)
+    static int saltSize = 16;
 
     public ShadowCipher(ShadowConfig config, byte[] salt) {
     }
 
+    //TODO(JAVA DOESNT HAVE COMPANION OBJECTSSSSSS!!!!!!)
     public static byte[] createSalt(ShadowConfig config) {
-        int saltSize = 0;
+        int saltSize;
         switch (config.cipherMode) {
             case AES_128_GCM: {
                 saltSize = 16;
                 break;
             }
             case AES_256_GCM:
-            case CHACHA20_IETF_POLY1305:{
+            case CHACHA20_IETF_POLY1305: {
                 saltSize = 32;
                 break;
             }
@@ -23,8 +30,12 @@ public class ShadowCipher {
                 throw new IllegalStateException("Unexpected value: " + config.cipherMode);
         }
         byte[] salt = new byte[saltSize];
-        Random rando = new Random();
-        return rando.nextBytes(salt);
+        Random random = new Random();
+        random.nextBytes(salt);
+        return salt;
     }
 
+    public byte[] pack(byte[] bytesToSend) {
+        return bytesToSend;
+    }
 }
--- app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowOutputStream.java
+++ app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowOutputStream.java
@@ -1,4 +1,42 @@
 package org.operatorfoundation.shadow;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
 public class ShadowOutputStream {
+    final OutputStream outputStream;
+    final ShadowCipher encryptionCipher;
+    byte[] buffer = new byte[0];
+
+    public ShadowOutputStream(OutputStream outputStream, ShadowCipher encryptionCipher) {
+        this.outputStream = outputStream;
+        this.encryptionCipher = encryptionCipher;
+    }
+
+
+    public void write(int b) throws IOException {
+        byte[] plainText = new byte[b];
+        write(plainText);
+    }
+
+    public void write(byte[] b) throws IOException {
+        if (b != null && b.length > 0) {
+            //TODO(it wont let me use the += but maybe the below method is possibly effective)
+            buffer = b;
+            while (buffer.length > 0) {
+                int numBytesToSend = Integer.min(ShadowCipher.maxPayloadSize, buffer.length);
+                //TODO(since sliceArray isn't here, this is what i found to work)
+                //TODO(copyOfRange is inclusive while the original was inclusive.  double check that the -1 is correct)
+                byte[] bytesToSend = Arrays.copyOfRange(buffer, numBytesToSend, buffer.length - 1);
+
+                byte[] cipherText = encryptionCipher.pack(bytesToSend);
+                outputStream.write(cipherText);
+            }
+        }
+    }
+
+    public void flush() throws IOException {
+        outputStream.flush();
+    }
 }
