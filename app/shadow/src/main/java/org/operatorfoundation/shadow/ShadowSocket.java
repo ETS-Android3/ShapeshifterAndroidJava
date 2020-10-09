1a7e23a26d9d225600911c165181581bc6bfffad
Bluesaxorcist <joshua@operatorfoundation.org>
2020-10-09 11:55:57 -0500

    finished the groundwork for shadow socket and started shadow socket channel

--- app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowSocket.java
+++ app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowSocket.java
@@ -2,17 +2,18 @@ package org.operatorfoundation.shadow;
 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Proxy;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketException;
+import java.nio.channels.SocketChannel;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.NoSuchPaddingException;
-
-import static org.operatorfoundation.shadow.ShadowCipher.config;
 
 // This class implements client sockets (also called just "sockets").
 public class ShadowSocket {
@@ -22,15 +23,7 @@ public class ShadowSocket {
     static ShadowCipher decryptionCipher;
     static Boolean connectionStatus;
 
-    // A socket is an endpoint for communication between two machines.
-    public ShadowSocket(ShadowConfig config) throws NoSuchAlgorithmException {
-        // Init block:
-        {
-            // Create salt for encryptionCipher
-            byte[] salt = ShadowCipher.createSalt(config);
-            // Create an encryptionCipher
-            encryptionCipher = new ShadowCipher(config, salt);
-        }
+    ShadowSocket(ShadowConfig config) {
     }
 
     // Constructors:
@@ -72,229 +65,214 @@ public class ShadowSocket {
         socket = new Socket(proxy);
     }
 
-    // Public functions:
-    // Binds the socket to a local address.
-    public void bind(SocketAddress bindpoint) throws IOException {
-        socket.bind(bindpoint);
-    }
+    // A socket is an endpoint for communication between two machines.
+    public Socket main(final ShadowConfig config) throws NoSuchAlgorithmException {
+        return new Socket() {
+            // Init block:
+            {
+                // Create salt for encryptionCipher
+                byte[] salt = ShadowCipher.createSalt(config);
+                // Create an encryptionCipher
+                encryptionCipher = new ShadowCipher(config, salt);
+            }
 
-    // Closes this socket.
-    public void close() throws IOException {
-        socket.close();
-    }
 
-    // Connects this socket to the server and initiates the handshake.
-    public void connect(SocketAddress endpoint) throws IOException, NoSuchAlgorithmException {
-        socket.connect(endpoint);
-        if (connectionStatus) {
-            throw new IOException();
-        }
-        connectionStatus = true;
-        handshake();
-    }
+            // Public functions:
+            // Binds the socket to a local address.
+            public void bind(SocketAddress bindpoint) throws IOException {
+                socket.bind(bindpoint);
+            }
 
-    // Connects this socket to the server with a specified timeout value and initiates the handshake.
-    public void connect(SocketAddress endpoint, int timeout) throws IOException, NoSuchAlgorithmException {
-        socket.connect(endpoint, timeout);
-        if (connectionStatus) {
-            throw new IOException();
-        }
-        handshake();
-    }
+            // Closes this socket.
+            public void close() throws IOException {
+                socket.close();
+            }
 
-    //TODO(i smell a recurring problem starting below)
+            // Connects this socket to the server and initiates the handshake.
+            public void connect(SocketAddress endpoint) throws IOException {
+                socket.connect(endpoint);
+                if (connectionStatus) {
+                    throw new IOException();
+                }
+                connectionStatus = true;
+                handshake();
+            }
 
-    // Returns the unique SocketChannel object associated with this socket, if any.
-//    public SocketChannel getChannel() {
-//        return new socket.channel;
-//    }
+            // Connects this socket to the server with a specified timeout value and initiates the handshake.
+            public void connect(SocketAddress endpoint, int timeout) throws IOException {
+                socket.connect(endpoint, timeout);
+                if (connectionStatus) {
+                    throw new IOException();
+                }
+                handshake();
+            }
 
-    // Returns the address to which the socket is connected.
-//    public InetAddress getInetAddress() {
-//        return socket.inetAddress;
-//    }
+            //TODO(some methods dont have the variables i.e: socket.channel like they do in kotlin)
 
-    // Returns an input stream and the decryption cipher for this socket.
-//    public InputStream getInputStream() throws Throwable {
-//        ShadowCipher cipher = decryptionCipher;
-//        if (cipher != null) {
-//            return ShadowInputStream(socket.inputStream, cipher);
-//        }
-//        throw new IOException();
-//    }
+            // Returns the unique SocketChannel object associated with this socket, if any.
+            public SocketChannel getChannel() {
+                return socket.getChannel();
+            }
 
-    // Tests if SO_KEEPALIVE is enabled.
-    public Boolean getKeepAlive() {
-        return false;
-    }
+            // Returns the address to which the socket is connected.
+            public InetAddress getInetAddress() {
+                return socket.getInetAddress();
+            }
 
-    // Gets the local address to which the socket is bound.
-//    public InetAddress getLocalAddress() {
-//        return socket.inetAddress;
-//    }
+            // Returns an input stream and the decryption cipher for this socket.
+//            public InputStream getInputStream() throws IOException {
+//                ShadowCipher cipher = decryptionCipher;
+//                if (cipher != null) {
+//                    return new ShadowInputStream(socket.getInputStream(), cipher);
+//                }
+//                throw new IOException();
+//            }
 
-    // Returns the local port number to which this socket is bound.
-//    public int getLocalPort() {
-//        return socket.localPort;
-//    }
+            // Tests if SO_KEEPALIVE is enabled.
+            public boolean getKeepAlive() {
+                return false;
+            }
 
-    // Returns the address of the endpoint this socket is bound to.
-//    public SocketAddress getLocalSocketAddress() {
-//        return socket.localSocketAddress;
-//    }
+            // Gets the local address to which the socket is bound.
+            public InetAddress getLocalAddress() {
+                return socket.getLocalAddress();
+            }
 
-    // Tests if SO_OOBINLINE is enabled.
-    public Boolean getOOBInline() {
-        return false;
-    }
+            // Returns the local port number to which this socket is bound.
+            public int getLocalPort() {
+                return socket.getLocalPort();
+            }
 
-    // Returns an output stream and the encryption cipher for this socket.
-//    public OutputStream getOutputStream() {
-//        return ShadowOutputStream(socket.outputStream, encryptionCipher);
-//    }
+            // Returns the address of the endpoint this socket is bound to.
+            public SocketAddress getLocalSocketAddress() {
+                return socket.getLocalSocketAddress();
+            }
 
-    // Returns the remote port number to which this socket is connected.
-//    public int getPort() {
-//        return socket.port;
-//    }
+            // Tests if SO_OOBINLINE is enabled.
+            public boolean getOOBInline() {
+                return false;
+            }
 
-    // Gets the value of the SO_RCVBUF option for this Socket, that is the buffer size used by the platform for input on this Socket.
-//    public int getReceiveBufferSize() {
-//        return socket.receiveBufferSize;
-//    }
+            // Returns an output stream and the encryption cipher for this socket.
+//            public OutputStream getOutputStream() throws IOException {
+//                return new ShadowOutputStream(socket.getOutputStream(), encryptionCipher);
+//            }
 
-    // Returns the address of the endpoint this socket is connected to, or null if it is unconnected.
-//    public SocketAddress getRemoteSocketAddress() {
-//        return socket.remoteSocketAddress;
-//    }
+            // Returns the remote port number to which this socket is connected.
+            public int getPort() {
+                return socket.getPort();
+            }
 
-    // Tests if SO_REUSEADDR is enabled.
-    public Boolean getReuseAddress() {
-        return false;
-    }
+            // Gets the value of the SO_RCVBUF option for this Socket, that is the buffer size used by the platform for input on this Socket.
+            public int getReceiveBufferSize() throws SocketException {
+                return socket.getReceiveBufferSize();
+            }
 
-    // Get value of the SO_SNDBUF option for this Socket, that is the buffer size used by the platform for output on this Socket.
-//    public int getSendBufferSize() {
-//        return socket.sendBufferSize;
-//    }
+            // Returns the address of the endpoint this socket is connected to, or null if it is unconnected.
+            public SocketAddress getRemoteSocketAddress() {
+                return socket.getRemoteSocketAddress();
+            }
 
-    // Returns setting for SO_LINGER. -1 implies that the option is disabled.
-    public int getSoLinger() {
-        return -1;
-    }
+            // Tests if SO_REUSEADDR is enabled.
+            public boolean getReuseAddress() {
+                return false;
+            }
 
-    // Returns setting for SO_TIMEOUT. 0 returns implies that the option is disabled (i.e., timeout of infinity).
-    public int getSoTimeout() {
-        return 0;
-    }
+            // Get value of the SO_SNDBUF option for this Socket, that is the buffer size used by the platform for output on this Socket.
+            public int getSendBufferSize() throws SocketException {
+                return socket.getSendBufferSize();
+            }
 
-    // Tests if TCP_NODELAY is enabled.
-    public Boolean getTcpNoDelay() {
-        return false;
-    }
+            // Returns setting for SO_LINGER. -1 implies that the option is disabled.
+            public int getSoLinger() {
+                return -1;
+            }
 
-    // Gets traffic class or type-of-service in the IP header for packets sent from this Socket.
-    public int getTrafficClass() throws SocketException {
-        throw new SocketException();
-    }
+            // Returns setting for SO_TIMEOUT. 0 returns implies that the option is disabled (i.e., timeout of infinity).
+            public int getSoTimeout() {
+                return 0;
+            }
 
-    // Returns the binding state of the socket.
-//    public Boolean isBound() {
-//        return socket.isBound;
-//    }
+            // Tests if TCP_NODELAY is enabled.
+            public boolean getTcpNoDelay() {
+                return false;
+            }
 
-    // Returns the closed state of the socket.
-//    public Boolean isClosed() {
-//        return socket.isClosed;
-//    }
+            // Gets traffic class or type-of-service in the IP header for packets sent from this Socket.
+            public int getTrafficClass() throws SocketException {
+                throw new SocketException();
+            }
 
-    // Returns the connection state of the socket.
-//    public Boolean isConnected() {
-//        return socket.isConnected;
-//    }
+            // Returns the binding state of the socket.
+            public boolean isBound() {
+                return socket.isBound();
+            }
 
-    // Returns whether the read-half of the socket connection is closed.
-//    public Boolean isInputShutdown() {
-//        return socket.isInputShutdown;
-//    }
+            // Returns the closed state of the socket.
+            public boolean isClosed() {
+                return socket.isClosed();
+            }
 
-    // Returns whether the write-half of the socket connection is closed.
-//    public Boolean isOutputShutdown() {
-//        return socket.isOutputShutdown;
-//    }
+            // Returns the connection state of the socket.
+            public boolean isConnected() {
+                return socket.isConnected();
+            }
 
-    // Send one byte of urgent data on the socket.
-    public void sendUrgentData(int data) {
-    }
+            // Returns whether the read-half of the socket connection is closed.
+            public boolean isInputShutdown() {
+                return socket.isInputShutdown();
+            }
 
-    // Enable/disable SO_KEEPALIVE.
-    public void setKeepAlive(Boolean on) {
-    }
+            // Returns whether the write-half of the socket connection is closed.
+            public boolean isOutputShutdown() {
+                return socket.isOutputShutdown();
+            }
 
-    // Enable/disable SO_OOBINLINE (receipt of TCP urgent data) By default, this option is disabled and TCP urgent data received on a socket is silently discarded.
-    public void setOOBInline(Boolean on) {
-    }
+            // Send one byte of urgent data on the socket.
+            public void sendUrgentData(int data) {
+            }
 
-    // Sets performance preferences for this socket.
-    public void setPerformancePreference(int connectionTime, int latency, int bandwidth) {
-    }
+            // Sets the SO_RCVBUF option to the specified value for this Socket.
+            public void setReceiveBufferSize(int size) throws SocketException {
+                socket.setSendBufferSize(size);
+            }
 
-    // Sets the SO_RCVBUF option to the specified value for this Socket.
-//    public void setReceiveBufferSize(int size) {
-//        socket.sendBufferSize = size;
-//    }
+            // Sets the SO_SNDBUF option to the specified value for this Socket.
+            public void setSendBufferSize(int size) throws SocketException {
+                socket.setSendBufferSize(size);
+            }
 
-    // Enable/disable the SO_REUSEADDR socket option.
-    public void setReuseAddress(Boolean on) {
-    }
+            // Enable/disable SO_TIMEOUT with the specified timeout, in milliseconds.
+            public void setSoTimeout(int timeout) {
+            }
 
-    // Sets the SO_SNDBUF option to the specified value for this Socket.
-//    public void setSendBufferSize(int size) {
-//        socket.sendBufferSize = size;
-//    }
+            // Sets traffic class or type-of-service octet in the IP header for packets sent from this Socket.
+            public void setTrafficClass(int tc) throws SocketException {
+                throw new SocketException();
+            }
 
-    // Enable/disable SO_LINGER with the specified linger time in seconds.
-    public void setSoLinger(Boolean on, int linger) {
-    }
-
-    // Enable/disable SO_TIMEOUT with the specified timeout, in milliseconds.
-    public void setSoTimeout(int timeout) {
-    }
-
-    // Enable/disable TCP_NODELAY (disable/enable Nagle's algorithm).
-    public void setTcpNoDelay(Boolean on) {
-    }
-
-    // Sets traffic class or type-of-service octet in the IP header for packets sent from this Socket.
-    public void setTrafficClass(int tc) throws SocketException {
-        throw new SocketException();
-    }
-
-    // Places the input stream for this socket at "end of stream".
-    public void shutDownInput() throws IOException {
-        socket.shutdownInput();
-    }
-
-    // Disables the output stream for this socket.
-    public void shutDownOutput() throws IOException {
-        socket.shutdownOutput();
-    }
-
-    // Converts this socket to a String.
-    public String toString() {
-        return "ShadowSocket[" + "password = " + config.password + ", cipherName = " + config.cipherName + "]";
+            // Converts this socket to a String.
+            @Override
+            public String toString() {
+                return "ShadowSocket[" + "password = " + config.password + ", cipherName = " + config.cipherName + "]";
+            }
+        };
     }
 
     // Private functions:
     // Exchanges the salt.
-    private void handshake() throws IOException, NoSuchAlgorithmException {
+    private void handshake() throws IOException {
         sendSalt();
-        receiveSalt();
+        try {
+            receiveSalt();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     // Sends the salt through the output stream.
     private void sendSalt() {
-        //socket.outputStream.write(encryptionCipher.salt);
+//        socket.outputStream.write(encryptionCipher.salt);
     }
 
     // Receives the salt through the input stream.
@@ -307,4 +285,3 @@ public class ShadowSocket {
 //        }
     }
 }
-
--- app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowSocketChannel.java
+++ app/shadow/src/main/java/org/operatorfoundation/shadow/ShadowSocketChannel.java
@@ -1,4 +1,256 @@
 package org.operatorfoundation.shadow;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.security.NoSuchAlgorithmException;
+import java.util.Set;
+
+import static org.operatorfoundation.shadow.ShadowCipher.config;
+
 public class ShadowSocketChannel {
+
+    public boolean connectionStatus = false;
+    public SocketChannel socketChannel = SocketChannel.open();
+    public ShadowCipher encryptionCipher;
+    public ShadowCipher decryptionCipher;
+
+    public ShadowSocketChannel(ShadowCipher encryptionCipher, ShadowCipher decryptionCipher) throws IOException {
+        this.encryptionCipher = encryptionCipher;
+        this.decryptionCipher = decryptionCipher;
+    }
+
+    public SocketChannel main(SelectorProvider selectorProvider, final ShadowConfig config) throws NoSuchAlgorithmException {
+        return new SocketChannel(selectorProvider) {
+            // Init block:
+            {
+                // Create salt for encryptionCipher
+                byte[] salt = ShadowCipher.createSalt(config);
+                // Create an encryptionCipher
+                encryptionCipher = new ShadowCipher(config, salt);
+            }
+
+            @Override
+            public SocketChannel bind(SocketAddress local) throws IOException {
+                return socketChannel.bind(local);
+            }
+
+            @Override
+            public boolean connect(SocketAddress remote) throws IOException {
+                socketChannel = open(remote);
+                if (connectionStatus) {
+                    return false;
+                }
+                connectionStatus = true;
+                try {
+                    handshake();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean finishConnect() throws IOException {
+                return connectionStatus;
+            }
+
+            @Override
+            public SocketAddress getLocalAddress() throws IOException {
+                return socketChannel.getLocalAddress();
+            }
+
+            @Override
+            public <T> T getOption(SocketOption<T> name) throws IOException {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public SocketAddress getRemoteAddress() throws IOException {
+                return socketChannel.getRemoteAddress();
+            }
+
+            @Override
+            protected void implCloseSelectableChannel() throws IOException {
+            }
+
+            @Override
+            protected void implConfigureBlocking(boolean block) throws IOException {
+            }
+
+            @Override
+            public boolean isConnected() {
+                return connectionStatus;
+            }
+
+            @Override
+            public boolean isConnectionPending() {
+                boolean isConnectionPending = false;
+                try {
+                    isConnectionPending = finishConnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return isConnectionPending;
+            }
+
+            @Override
+            public int read(ByteBuffer dst) throws IOException {
+//                byte[] buffer = new byte[0];
+//                byte[] data = new byte[(dst.capacity())];
+//                if (data.length == 0) {
+//                    return 0;
+//                }
+//
+//                // puts bytes in a buffer.
+//                //TODO(needs to do the thing but in java)
+//                if (data.length <= buffer.length) {
+//                    int resultSize = Integer.min(data.length, buffer.length);
+//                    buffer.copyInto(data, 0, 0, resultSize);
+//                    buffer.sliceArray(resultSize + 1, buffer.length - 1);
+//
+//                    return resultSize;
+//                }
+//
+//                // get encrypted length.
+//                int lengthDataSize = ShadowCipher.lengthWithTagSize;
+//
+//                // read bytes up to the size of encrypted lengthSize into a byte buffer.
+//                ByteBuffer encryptedLengthData = Utility.readNBytes(socketChannel, lengthDataSize);
+//
+//                // decrypt encrypted length to find out payload length.
+//                byte[] lengthData = new byte[0];
+//                try {
+//                    lengthData = decryptionCipher.decrypt(new byte[encryptedLengthData.capacity()]);
+//                } catch (InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                // change lengthData from  BigEndian representation to int length.
+//                byte leftByte = lengthData[0];
+//                byte rightByte = lengthData[1];
+//                int payloadLength = ((int)leftByte * 256) + (int)rightByte;
+//
+//                // read and decrypt payload with the resulting length.
+//                ByteBuffer encryptedPayload = Utility.readNBytes(socketChannel, payloadLength + ShadowCipher.tagSize);
+//                byte[] payload = new byte[0];
+//                try {
+//                    payload = decryptionCipher.decrypt(new byte[encryptedPayload.capacity()]);
+//                } catch (InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                // put payload into buffer
+//                //TODO(more byte array witchcraft that i cant find a java equivalent for)
+//                buffer = buffer + payload;
+//                int resultSize = Integer.min(data.length, buffer.length);
+//                buffer.copyInto(data, 0, 0, resultSize);
+//
+//                // take bytes out of buffer
+//                buffer.sliceArray(resultSize + 1, buffer.length - 1);
+//
+//                return resultSize;
+                return 0;
+            }
+
+            @Override
+            public long read(ByteBuffer[] dsts, int offset, int length) throws IOException {
+                //TODO(there's a lot of stuff in the kotlin version that java just dont do)
+//                dsts?.get(offset)?.remaining()
+//                dsts?.get(offset + 1)?.remaining()
+//                val remaining = dsts?.get(offset + length - 1)?.remaining()
+//                return remaining!!.toLong()
+                return 0;
+            }
+
+            @Override
+            public <T> SocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+                return socketChannel.setOption(name, value);
+            }
+
+            @Override
+            public SocketChannel shutdownInput() throws IOException {
+                return socketChannel.shutdownInput();
+            }
+
+            @Override
+            public SocketChannel shutdownOutput() throws IOException {
+                return socketChannel.shutdownOutput();
+            }
+
+            @Override
+            public Socket socket() {
+                return socket();
+            }
+
+            @Override
+            public Set<SocketOption<?>> supportedOptions() {
+                //TODO(dont have muteableSetOf in java)
+                return null;
+            }
+
+            @Override
+            public int write(ByteBuffer src) throws IOException {
+                byte[] buffer = new byte[0];
+                byte[] data = new byte[src.capacity()];
+                if (data.length == 0) {
+                    return 0;
+                }
+
+                // put into buffer.
+                //TODO(+= and byte arrays is illegal in java?)
+                //buffer = buffer + data;
+
+                // keep writing until the buffer is empty in case user exceeds maximum.
+                while (buffer.length > 0) {
+                    int numBytesToSend = Integer.min(ShadowCipher.maxPayloadSize, buffer.length);
+
+                    // make a copy of the buffer
+                    //byte[] bytesToSend = buffer.copyOfRange(0, numBytesToSend);
+
+                    // take bytes out of buffer.
+                    //buffer = buffer.sliceArray(numBytesToSend, buffer.length - 1);
+
+                    //byte[] cipherText = encryptionCipher.pack(bytesToSend);
+
+                    //socketChannel.write(ByteBuffer.wrap(cipherText));
+                }
+                return src.remaining();
+            }
+
+            @Override
+            public long write(ByteBuffer[] srcs, int offset, int length) throws IOException {
+                //TODO(moar byte array witchcraft)
+//                srcs?.get(offset)?.remaining()
+//                srcs?.get(offset + 1)?.remaining()
+//                val remaining = srcs?.get(offset + length - 1)?.remaining()
+//                return remaining!!.toLong()
+                return 0;
+            }
+        };
+    }
+
+    public void handshake() throws IOException, NoSuchAlgorithmException {
+        sendSalt();
+        receiveSalt();
+    }
+
+    public void sendSalt() throws IOException {
+        ByteBuffer buffer = ByteBuffer.wrap(encryptionCipher.salt);
+        socketChannel.write(buffer);
+    }
+
+    public void receiveSalt() throws IOException, NoSuchAlgorithmException {
+        SocketChannel channel = socketChannel;
+        ByteBuffer result = Utility.readNBytes(channel, ShadowCipher.saltSize);
+        if (result.position() == encryptionCipher.salt.length) {
+            decryptionCipher = new ShadowCipher(config, result.array());
+        } else {
+            throw new IOException();
+        }
+    }
 }
