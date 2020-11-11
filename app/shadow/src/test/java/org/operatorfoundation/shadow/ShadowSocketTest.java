37b68a0a95e43eda495fe78e397f11d77d62f8cf
Bluesaxorcist <joshua@operatorfoundation.org>
2020-11-11 09:41:27 -0600

    added libsodium test

--- app/shadow/src/test/java/org/operatorfoundation/shadow/ShadowSocketTest.java
+++ app/shadow/src/test/java/org/operatorfoundation/shadow/ShadowSocketTest.java
@@ -8,6 +8,8 @@ import org.bouncycastle.crypto.params.AEADParameters;
 import org.bouncycastle.crypto.tls.Chacha20Poly1305;
 import org.bouncycastle.jcajce.spec.AEADParameterSpec;
 import org.junit.Test;
+import org.libsodium.jni.NaCl;
+import org.libsodium.jni.Sodium;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -169,6 +171,31 @@ public class ShadowSocketTest {
         assertEquals("Yo", new String(buffer));
     }
 
+    @Test
+    public void libsodiumTest() {
+        String message = "Test Message";
+        Sodium sodium = NaCl.sodium();
+
+        byte[] nonce = new byte[Sodium.crypto_aead_chacha20poly1305_npubbytes()];
+        byte[] key = new byte[Sodium.crypto_aead_chacha20poly1305_keybytes()];
+        Sodium.randombytes_buf(nonce, nonce.length);
+        Sodium.randombytes_buf(key, key.length);
+
+        int mlen = message.length();
+        int[] clen_p = new int[0];
+        byte[] c = new byte[mlen + Sodium.crypto_aead_chacha20poly1305_abytes()];
+
+        int encryptReturn = Sodium.crypto_aead_chacha20poly1305_encrypt(c, clen_p, message.getBytes(), mlen, new byte[0], 0, new byte[0], nonce, key);
+
+        assertEquals(0, encryptReturn);
+
+        byte[] m = new byte[message.length()];
+
+        int[] mlen_p = new int[1];
+
+        Sodium.crypto_aead_chacha20poly1305_decrypt(m, mlen_p, new byte[0], c, c.length, new byte[0], 0, nonce, key);
+        assertEquals(message, new String(m));
+    }
 
 }
 
