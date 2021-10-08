4114b438fdd6795a50b544510e14f7379dc115c2
CryptoSax <joshua@operatorfoundation.org>
2021-10-08 14:57:44 -0500

    added DarkStar

---
+++ Shadow/src/main/java/org/operatorfoundation/shapeshifter/shadow/java/DarkStar.java
@@ -0,0 +1,268 @@
+package org.operatorfoundation.shapeshifter.shadow.java;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.*;
+
+
+public class DarkStar {
+    public static byte[] iv = new SecureRandom().generateSeed(16);
+    public static int P256KeySize = 32;
+    public static int ConfirmationSize = 32;
+    public SecretKey encryptKey;
+    public SecretKey decryptKey;
+    static byte[] darkStarBytes = "DarkStar".getBytes();
+    static byte[] clientStringBytes = "client".getBytes();
+    static byte[] serverStringBytes = "server".getBytes();
+
+    public DarkStar(InputStream inputStream, OutputStream outputStream, String host, int port, KeyPair serverPersistentKey) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        // generate an ephemeral keypair
+        KeyPair clientEphemeralKey = generateECKeys();
+
+        // get the client private and public key
+        PrivateKey clientEphemeralPrivateKey = null;
+        PublicKey clientEphemeralPublicKey = null;
+        if (clientEphemeralKey != null) {
+            clientEphemeralPrivateKey = clientEphemeralKey.getPrivate();
+            clientEphemeralPublicKey = clientEphemeralKey.getPublic();
+        }
+        if (!(clientEphemeralPublicKey instanceof BCECPublicKey)) {
+            System.out.println("could not typecast to bcec");
+        }
+
+        // convert the public key into data to be sent to the server
+        byte[] clientEphemeralPublicKeyData = publicKeyToBytes(clientEphemeralPublicKey);
+
+        // send the public key to the server
+        outputStream.write(clientEphemeralPublicKeyData);
+        
+        // receive the ephemeral public key from the server
+        byte[] serverEphemeralPublicKeyData = new byte[P256KeySize];
+        int serverBytesRead = inputStream.read(serverEphemeralPublicKeyData);
+        if (serverBytesRead != P256KeySize) {
+            Log.e("DarkStar", "Failed to read the server's ephemeral public key data");
+        }
+
+        // turn the server's public key data back to a public key type
+        PublicKey serverEphemeralPublicKey = bytesToPublicKey(serverEphemeralPublicKeyData);
+
+        // derive shared keys
+        // FIXME: make separate function for encrypt and decrypt key
+        encryptKey = generateSharedKeyClient(host, port, clientEphemeralKey, serverEphemeralPublicKey, serverPersistentKey.getPublic());
+        decryptKey = generateSharedKeyServer(host, port, serverEphemeralKey, serverPersistentKey, clientEphemeralPublicKey);
+
+        // Generate client confirmation code
+        // byte[] clientConfirmationCode = generateClientConfirmationCode(host, port, serverEphemeralPublicKey, clientEphemeralPublicKey, );
+
+        // Receive server confirmation code
+        byte[] serverConfirmationCode = new byte[ConfirmationSize];
+        int serverConfirmationBytesRead = inputStream.read(serverConfirmationCode);
+        if (serverConfirmationBytesRead != ConfirmationSize) {
+            Log.e("DarkStar", "Failed to read the server's confirmation code");
+        }
+
+//        if (serverConfirmationCode != clientConfirmationCode) {
+//            Log.e("DarkStar", "client and server confirmation code do not match");
+//        }
+    }
+
+    public static KeyPair generateECKeys() {
+        try {
+            ECNamedCurveParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec("secp256r1");
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(
+                    "EC", new BouncyCastleProvider());
+
+            keyPairGenerator.initialize(parameterSpec);
+
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static KeyPair loadECKeys(String privateKeyString, String publicKeyString) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("EC", new BouncyCastleProvider());
+
+            byte[] privateKeyBytes = hexToBytes(privateKeyString);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            PrivateKey privateKey = keyFactory.generatePrivate(spec);
+
+            byte[] publicKeyBytes = hexToBytes(publicKeyString);
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+            return new KeyPair(publicKey, privateKey);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static SecretKey generateSharedSecret(PrivateKey privateKey, PublicKey publicKey) {
+        try {
+            KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH", new BouncyCastleProvider());
+            keyAgreement.init(privateKey);
+            keyAgreement.doPhase(publicKey, true);
+
+            SecretKey key = keyAgreement.generateSecret("secp256r1");
+            return key;
+        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static SecretKey generateSharedKeyServer(String host, int port, KeyPair serverEphemeral, KeyPair serverPersistent, PublicKey clientEphemeralPublicKey) throws UnknownHostException, NoSuchAlgorithmException {
+        SecretKey ecdh1 = DarkStar.generateSharedSecret(serverEphemeral.getPrivate(), clientEphemeralPublicKey);
+        SecretKey ecdh2 = DarkStar.generateSharedSecret(serverPersistent.getPrivate(), clientEphemeralPublicKey);
+        byte[] serverIdentifier = DarkStar.makeServerIdentifier(host, port);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(ecdh1.getEncoded());
+        digest.update(ecdh2.getEncoded());
+        digest.update(serverIdentifier);
+        digest.update(publicKeyToBytes(clientEphemeralPublicKey));
+        digest.update(publicKeyToBytes(serverEphemeral.getPublic()));
+        digest.update(darkStarBytes);
+        byte[] result = digest.digest();
+
+        return new SecretKeySpec(result, 0, result.length, "AES");
+    }
+
+    public static SecretKey generateSharedKeyClient(String host, int port, KeyPair clientEphemeral, PublicKey serverEphemeralPublicKey, PublicKey serverPersistentPublicKey) throws UnknownHostException, NoSuchAlgorithmException {
+        SecretKey ecdh1 = DarkStar.generateSharedSecret(clientEphemeral.getPrivate(), serverEphemeralPublicKey);
+        SecretKey ecdh2 = DarkStar.generateSharedSecret(clientEphemeral.getPrivate(), serverPersistentPublicKey);
+        byte[] serverIdentifier = DarkStar.makeServerIdentifier(host, port);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(ecdh1.getEncoded());
+        digest.update(ecdh2.getEncoded());
+        digest.update(serverIdentifier);
+        digest.update(publicKeyToBytes(clientEphemeral.getPublic()));
+        digest.update(publicKeyToBytes(serverEphemeralPublicKey));
+        digest.update(darkStarBytes);
+        byte[] result = digest.digest();
+
+        return new SecretKeySpec(result, 0, result.length, "AES");
+    }
+
+    public static byte[] makeServerIdentifier(String host, int port) throws UnknownHostException {
+        InetAddress ip = InetAddress.getByName(host);
+        byte[] address = ip.getAddress();
+        ByteBuffer buf = ByteBuffer.allocate(2);
+        buf.putShort((short) port);
+        byte[] portBytes = buf.array();
+
+        return Utility.plusEqualsByteArray(address, portBytes);
+    }
+
+    public static byte[] generateServerConfirmationCode(String host, int port, PublicKey serverEphemeralPublicKey, PublicKey clientEphemeralPublicKey, SecretKey sharedKey) throws NoSuchAlgorithmException, UnknownHostException, InvalidKeyException {
+        byte[] serverIdentifier = makeServerIdentifier(host, port);
+        byte[] serverEphemeralPublicKeyData = serverEphemeralPublicKey.getEncoded();
+        byte[] clientEphemeralPublicKeyData = clientEphemeralPublicKey.getEncoded();
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(sharedKey.getEncoded(), "HmacSHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKeySpec);
+        mac.update(serverIdentifier);
+        mac.update(serverEphemeralPublicKeyData);
+        mac.update(clientEphemeralPublicKeyData);
+        mac.update(darkStarBytes);
+        mac.update(clientStringBytes);
+
+        return mac.doFinal();
+    }
+
+    public static byte[] generateClientConfirmationCode(String host, int port, PublicKey serverPersistentPublicKey, PublicKey clientEphemeralPublicKey, PublicKey ecdhPublicKey, PrivateKey ecdhPrivateKey) throws NoSuchAlgorithmException, UnknownHostException {
+        SecretKey sharedSecret = DarkStar.generateSharedSecret(ecdhPrivateKey, ecdhPublicKey);
+        byte[] serverIdentifier = makeServerIdentifier(host, port);
+        byte[] serverPersistentPublicKeyData = serverPersistentPublicKey.getEncoded();
+        byte[] clientEphemeralPublicKeyData = clientEphemeralPublicKey.getEncoded();
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(sharedSecret.getEncoded());
+        digest.update(serverIdentifier);
+        digest.update(serverPersistentPublicKeyData);
+        digest.update(clientEphemeralPublicKeyData);
+        digest.update(darkStarBytes);
+        digest.update(serverStringBytes);
+
+        return digest.digest();
+    }
+
+    public static byte[] publicKeyToBytes(PublicKey pubKey) {
+        if (!(pubKey instanceof BCECPublicKey)) {
+            System.out.println("could not typecast to bcec");
+        }
+        BCECPublicKey bcecPubKey = (BCECPublicKey) pubKey;
+        return bcecPubKey.getQ().getEncoded(true);
+    }
+
+    public static PublicKey bytesToPublicKey(byte[] bytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256r1");
+        ECPoint point = ecSpec.getCurve().decodePoint(bytes);
+        ECPublicKeySpec pubSpec = new ECPublicKeySpec(point, ecSpec);
+        KeyFactory kf = KeyFactory.getInstance("EC", new BouncyCastleProvider());
+        return kf.generatePublic(pubSpec);
+    }
+
+    public static String bytesToHex(byte[] data, int length) {
+        String digits = "0123456789ABCDEF";
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i != length; i++) {
+            int v = data[i] & 0xff;
+
+            buffer.append(digits.charAt(v >> 4));
+            buffer.append(digits.charAt(v & 0xf));
+        }
+
+        return buffer.toString();
+    }
+
+    public static String bytesToHex(byte[] data) {
+        return bytesToHex(data, data.length);
+    }
+
+    public static byte[] hexToBytes(String string) {
+        int length = string.length();
+        byte[] data = new byte[length / 2];
+        for (int i = 0; i < length; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(string.charAt(i), 16) << 4) + Character
+                    .digit(string.charAt(i + 1), 16));
+        }
+        return data;
+    }
+}
--- Shadow/src/test/java/org/operatorfoundation/shadow/ExampleUnitTest.java
+++ Shadow/src/test/java/org/operatorfoundation/shadow/ExampleUnitTest.java
@@ -1,7 +1,17 @@
 package org.operatorfoundation.shadow;
 
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jcajce.spec.AEADParameterSpec;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.interfaces.ECPublicKey;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.util.encoders.Base64;
+import org.junit.Assert;
 import org.junit.Test;
+import org.operatorfoundation.shapeshifter.shadow.java.DarkStar;
 import org.operatorfoundation.shapeshifter.shadow.java.ShadowConfig;
 import org.operatorfoundation.shapeshifter.shadow.java.ShadowSocket;
 import org.operatorfoundation.shapeshifter.shadow.java.TestServer;
@@ -11,11 +21,21 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
+import java.net.UnknownHostException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.PublicKey;
 import java.security.Security;
 import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.util.TreeSet;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -24,8 +44,18 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.bytesToHex;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.generateClientConfirmationCode;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.generateECKeys;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.generateServerConfirmationCode;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.generateSharedKeyClient;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.generateSharedKeyServer;
+import static org.operatorfoundation.shapeshifter.shadow.java.DarkStar.publicKeyToBytes;
+
+import android.util.Log;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -33,6 +63,22 @@ import static org.junit.Assert.assertNotNull;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    String clientEphemeralPrivateKeyString = "308193020100301306072A8648CE3D020106082A8648CE3D030107047930770201010420D27CDB0FA88F0FDF05E74AE7D05E50D21E23EE8DE24F18B2B06B031978040777A00A06082A8648CE3D030107A144034200048272A97431C1B78751B5DAF19F7B32ABF7824D8E47101F28BA5543154446969B9334018EBBCB77B2EFFC2A6B6EB2FD87DE5899E04EF8508015610659C84C5C60";
+    String clientEphemeralPublicKeyString = "3059301306072A8648CE3D020106082A8648CE3D030107034200048272A97431C1B78751B5DAF19F7B32ABF7824D8E47101F28BA5543154446969B9334018EBBCB77B2EFFC2A6B6EB2FD87DE5899E04EF8508015610659C84C5C60";
+    KeyPair clientEphemeralKey = DarkStar.loadECKeys(clientEphemeralPrivateKeyString, clientEphemeralPublicKeyString);
+
+    String serverEphemeralPrivateKeyString = "308193020100301306072A8648CE3D020106082A8648CE3D03010704793077020101042004A7065B1D249C0EDD7D5086A71955D233F04CD210F4AC22DD05264CFC0A001FA00A06082A8648CE3D030107A144034200049B2F18C37AC4176D1545B9E6B740DA755A42E617550AA1DDE67F865F24BED4FC901D93D30E62A400D1A4891878173C20C91B7CE754BB5CE27ABBDD5683D59BE0";
+    String serverEphemeralPublicKeyString = "3059301306072A8648CE3D020106082A8648CE3D030107034200049B2F18C37AC4176D1545B9E6B740DA755A42E617550AA1DDE67F865F24BED4FC901D93D30E62A400D1A4891878173C20C91B7CE754BB5CE27ABBDD5683D59BE0";
+    KeyPair serverEphemeralKey = DarkStar.loadECKeys(serverEphemeralPrivateKeyString, serverEphemeralPublicKeyString);
+
+    String serverPersistentPrivateKeyString = "308193020100301306072A8648CE3D020106082A8648CE3D03010704793077020101042005B4D9C2C7792964F6E7C01B17701366F83E649C17FB2673F708381970346BA0A00A06082A8648CE3D030107A144034200041FF393BB8D976A5098F4D88853F7EA7A47DF7E1717A7E18084F3E3CA8D0FA9ACFB0F0E18801638712006B041880C0A15D227614E255728FF06EC8B7E466E19D4";
+    String serverPersistentPublicKeyString = "3059301306072A8648CE3D020106082A8648CE3D030107034200041FF393BB8D976A5098F4D88853F7EA7A47DF7E1717A7E18084F3E3CA8D0FA9ACFB0F0E18801638712006B041880C0A15D227614E255728FF06EC8B7E466E19D4";
+    KeyPair serverPersistentKey = DarkStar.loadECKeys(serverPersistentPrivateKeyString, serverPersistentPublicKeyString);
+
+    byte[] darkStarBytes = "DarkStar".getBytes();
+    byte[] clientStringBytes = "client".getBytes();
+    byte[] serverStringBytes = "server".getBytes();
 
     @Test
     public void shadowSocketConstructor1TestAES128() throws IOException, NoSuchAlgorithmException {
@@ -152,6 +198,27 @@ public class ExampleUnitTest {
     }
 
     @Test
+    public void algorythmFinder() {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        TreeSet<String> algs = new TreeSet<>();
+        for (Provider provider : Security.getProviders()) {
+//            provider.getServices().stream()
+//                    .filter(s -> "Cipher".equals(s.getType()))
+//                    .map(Service::getAlgorithm)
+//                    .forEach(algs::add);
+            System.out.println(provider.getName());
+//            for (java.security.Provider.Service service : provider.getServices()) {
+//
+//                    System.out.println(service);
+//                    System.out.println(service.getType());
+//                    System.out.println(service.getAlgorithm());
+//            }
+        }
+        org.bouncycastle.asn1.x9.ECNamedCurveTable.getNames();
+        algs.stream().forEach(System.out::println);
+    }
+
+    @Test
     public void bouncyCastleTest() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
         Cipher cipher = Cipher.getInstance("CHACHA7539");
@@ -169,4 +236,108 @@ public class ExampleUnitTest {
         };
         byte[] encrypted = cipher.doFinal(plainText);
     }
+
+    @Test
+    public void newKeyParseTest() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] clientEphemeralBytes = DarkStar.publicKeyToBytes(clientEphemeralKey.getPublic());
+        PublicKey decodedClientEphemeralKey = DarkStar.bytesToPublicKey(clientEphemeralBytes);
+        assertEquals(decodedClientEphemeralKey, clientEphemeralKey.getPublic());
+
+        byte[] serverEphemeralBytes = DarkStar.publicKeyToBytes(serverEphemeralKey.getPublic());
+        PublicKey decodedServerEphemeralKey = DarkStar.bytesToPublicKey(serverEphemeralBytes);
+        assertEquals(decodedServerEphemeralKey, serverEphemeralKey.getPublic());
+
+        byte[] serverPersistentBytes = DarkStar.publicKeyToBytes(serverPersistentKey.getPublic());
+        PublicKey decodedServerPersistentKey = DarkStar.bytesToPublicKey(serverPersistentBytes);
+        assertEquals(decodedServerPersistentKey, serverPersistentKey.getPublic());
+    }
+
+    @Test
+    public void ecdhTest() {
+        SecretKey sharedSecret = DarkStar.generateSharedSecret(clientEphemeralKey.getPrivate(), serverPersistentKey.getPublic());
+        System.out.println("Format: ");
+        System.out.println(sharedSecret.getFormat());
+        System.out.println("encoded: ");
+        System.out.println(DarkStar.bytesToHex(sharedSecret.getEncoded()));
+
+        assertEquals(sharedSecret.getFormat(), "RAW");
+        assertEquals(sharedSecret.getEncoded().length, 32);
+    }
+
+    @Test
+    public void serverIdentifierTest() throws UnknownHostException {
+        byte[] correct = {127, 0, 0, 1, 4, (byte) 210};
+        byte[] serverIdentifier = DarkStar.makeServerIdentifier("127.0.0.1", 1234);
+        assertArrayEquals(correct, serverIdentifier);
+        System.out.println(bytesToHex(serverIdentifier));
+    }
+
+    @Test
+    public void privateKeyGenTest() {
+        KeyPair clientEphemeralKey = generateECKeys();
+        PrivateKey clientEphemeralPrivateKey = null;
+        clientEphemeralPrivateKey = clientEphemeralKey.getPrivate();
+        String format = clientEphemeralPrivateKey.getFormat();
+        Log.i("DarkStar", "getFormat result: " + format);
+        String encoded = DarkStar.bytesToHex(clientEphemeralPrivateKey.getEncoded(), clientEphemeralPrivateKey.getEncoded().length);
+        System.out.println("private: ");
+        System.out.println(encoded);
+        byte[] clientEphemeralKeyData = clientEphemeralKey.getPublic().getEncoded();
+        System.out.println("public: ");
+        System.out.println(DarkStar.bytesToHex(clientEphemeralKeyData, clientEphemeralKeyData.length));
+    }
+
+    @Test
+    public void darkStarPublicKeyTest() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
+// generate an ephemeral keypair
+
+        // get the client private and public key
+        PublicKey clientEphemeralPublicKey = null;
+        if (clientEphemeralKey != null) {
+            clientEphemeralPublicKey = clientEphemeralKey.getPublic();
+        }
+
+        if (!(clientEphemeralPublicKey instanceof BCECPublicKey)) {
+            System.out.println("could not typecast to bcec");
+        }
+        BCECPublicKey bcecPubKey = (BCECPublicKey) clientEphemeralPublicKey;
+        byte[] encodedKey = bcecPubKey.getQ().getEncoded(true);
+        DarkStar.bytesToHex(encodedKey, encodedKey.length);
+
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256r1");
+        ECPoint point = ecSpec.getCurve().decodePoint(encodedKey);
+        ECPublicKeySpec pubSpec = new ECPublicKeySpec(point, ecSpec);
+        KeyFactory kf = KeyFactory.getInstance("EC", new BouncyCastleProvider());
+        ECPublicKey myECPublicKey = (ECPublicKey) kf.generatePublic(pubSpec);
+        PublicKey decodedPublicKey = (PublicKey) myECPublicKey;
+
+        assertEquals(decodedPublicKey, clientEphemeralPublicKey);
+    }
+
+    @Test
+    public void sharedKeyGenTest() throws UnknownHostException, NoSuchAlgorithmException {
+        SecretKey sharedClientKey = generateSharedKeyClient("127.0.0.1", 1234, clientEphemeralKey, serverEphemeralKey.getPublic(), serverPersistentKey.getPublic());
+        SecretKey sharedServerKey = generateSharedKeyServer("127.0.0.1", 1234, serverEphemeralKey, serverPersistentKey, clientEphemeralKey.getPublic());
+        assertEquals(sharedClientKey, sharedServerKey);
+    }
+
+    @Test
+    public void serverConfirmationTest() throws NoSuchAlgorithmException, UnknownHostException, InvalidKeyException {
+        SecretKey sharedClientKey = generateSharedKeyClient("127.0.0.1", 1234, clientEphemeralKey, serverEphemeralKey.getPublic(), serverPersistentKey.getPublic());
+        SecretKey sharedServerKey = generateSharedKeyServer("127.0.0.1", 1234, serverEphemeralKey, serverPersistentKey, clientEphemeralKey.getPublic());
+
+        byte[] serverSide = generateServerConfirmationCode("127.0.0.1", 1234, serverEphemeralKey.getPublic(), clientEphemeralKey.getPublic(), sharedServerKey);
+        byte[] clientSide = generateServerConfirmationCode("127.0.0.1", 1234, serverEphemeralKey.getPublic(), clientEphemeralKey.getPublic(), sharedClientKey);
+
+        assertArrayEquals(serverSide, clientSide);
+    }
+
+    @Test
+    public void clientConfirmationTest() throws NoSuchAlgorithmException, UnknownHostException, InvalidKeyException {
+        byte[] serverSide = generateClientConfirmationCode("127.0.0.1", 1234, serverPersistentKey.getPublic(), clientEphemeralKey.getPublic(), clientEphemeralKey.getPublic(), serverPersistentKey.getPrivate());
+        byte[] clientSide = generateClientConfirmationCode("127.0.0.1", 1234, serverPersistentKey.getPublic(), clientEphemeralKey.getPublic(), serverPersistentKey.getPublic(), clientEphemeralKey.getPrivate());
+
+        assertArrayEquals(serverSide, clientSide);
+    }
+
 }
